852c0c923e3729b3cc1582b430fd360b436500df
Miguel Cervantes <migcerva@amazon.com>
2017-09-13 13:17:01 -0700

    File uploading works

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ app.server = http.createServer(app);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload());
 app.use(routes);
 
 app.server.listen(process.env.PORT || 7890, () => {
--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 // define the home page route
 router.get('/', (request, response) => {
-  response.send('Home page');
+  response.sendFile('index.html' , { root : __dirname});
+});
+
+router.post('/', (req, res) => {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+	sampleFile.mv('./uploads/file.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 });
 
 module.exports = router;
