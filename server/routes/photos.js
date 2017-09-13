63578c9798e82af13ba3afc74ad57fe5be44c161
Miguel Cervantes <migcerva@amazon.com>
2017-09-13 15:42:09 -0700

    Fixed spacing issue that could cause unexpected return

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   	if(!req.files)
-    	return res.status(400).send('No files were uploaded.');
+		return res.status(400).send('No files were uploaded.');
 	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   	let sampleFile = req.files.file;
 
